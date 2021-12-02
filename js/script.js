263fa23f8949267d77a5512aa1a00a54afa77fc5
Bisio98 <alberto.bisetti@hotmail.it>
2021-12-02 14:28:57 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        text: 'Guarda che bella foto',
+        source: 'https://images.agi.it/pictures/agi/agi/2016/03/17/154253836-ebce30d5-3e45-46f8-8dde-df97ac3e95e8.jpg'
+    }
+});
